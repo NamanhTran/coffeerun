747f0b6ce4abb2410466e00ac18459ee81c9aa78
Namanh Tran <NamanhDTran@gmail.com>
2021-10-01 22:21:05 -0700

    inital commit

---
+++ scripts/paymentMain.js
@@ -0,0 +1,35 @@
+(function (window) {
+    'use strict';
+    var FORM_SELECTOR = '[data-coffee-payment="form"]';
+    var App = window.App;
+    var FormHandler = App.FormHandler;
+    var formHandler = new FormHandler(FORM_SELECTOR);
+    formHandler.addSubmitHandler( function (data) {
+        var $div = $('<div></div>', {
+            'id': 'paymentModal',
+            'class': "modal"
+        });
+
+        var $p = $('<p></p>');
+        var $a = $('<a></a>', {
+            'href': "#",
+            'rel': "modal:close"
+        });
+
+        var description = 'Thank you for your payment, ';
+
+        if (data.title) {
+            description += data.title + ' ';
+        }
+
+        description += data.username;
+
+        $div.append($p);
+        $div.append($a);
+        $a.append("Close");
+        $p.append(description);
+
+        $('body').append($div);
+        $("#paymentModal").modal();
+    });
+})(window);
